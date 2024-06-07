93fcedaad16a595bcfb493ac7cb2c15080ff9818
DhanushRamS <dhanushram970@gmail.com>
2024-06-07 15:55:00 +0530

    VendorScanner Changes

--- plasticart/src/Components/pageviews/VendorScanner.js
+++ plasticart/src/Components/pageviews/VendorScanner.js
@@ -1,93 +1,66 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./VendorStylescn.module.css";
-import { appAuth, appFirestore } from "../../config";
-import { collection, getDocs, setDoc, doc } from "firebase/firestore";
+import { appFirestore } from "../../config";
+import { setDoc, doc } from "firebase/firestore";
 
 function VendorScanner() {
   const [capturedImage, setCapturedImage] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [prediction, setPrediction] = useState(null);
   const [verificationResult, setVerificationResult] = useState(null);
-  const videoRef = useRef(null);
   const location = useLocation();
   const navigate = useNavigate();
 
   const { pickup } = location.state; // Get pickup data from location state
 
-  useEffect(() => {
-    const setupCamera = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-        });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-        }
-      } catch (error) {
-        console.error("Error accessing camera:", error);
-      }
-    };
+  const handleImageUpload = async (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = async () => {
+        const imageData = reader.result;
+        const blobData = dataURItoBlob(imageData);
 
-    setupCamera();
+        navigator.geolocation.getCurrentPosition(
+          async (position) => {
+            const { latitude, longitude } = position.coords;
 
-    return () => {
-      if (videoRef.current && videoRef.current.srcObject) {
-        const tracks = videoRef.current.srcObject.getTracks();
-        tracks.forEach((track) => track.stop());
-      }
-    };
-  }, []);
+            const formData = new FormData();
+            formData.append("image", blobData);
+            formData.append("lat", latitude);
+            formData.append("long", longitude);
 
-  const handleCaptureClick = async () => {
-    const video = videoRef.current;
-    const canvas = document.createElement("canvas");
-    const context = canvas.getContext("2d");
+            try {
+              const response = await axios({
+                method: "post",
+                url: "https://dhanush10.pythonanywhere.com/upload",
+                data: formData,
+                headers: { "Content-Type": "multipart/form-data" },
+              });
 
-    canvas.width = 280;
-    canvas.height = 200;
+              const data = response.data;
+              setCapturedImage({
+                image: imageData,
+                prediction: data.prediction,
+                latitude,
+                longitude,
+              });
 
-    context.drawImage(video, 0, 0, 280, 200);
-
-    const imageData = canvas.toDataURL("image/jpeg");
-    const blobData = dataURItoBlob(imageData);
-
-    navigator.geolocation.getCurrentPosition(
-      async (position) => {
-        const { latitude, longitude } = position.coords;
-
-        const formData = new FormData();
-        formData.append("image", blobData);
-        formData.append("lat", latitude);
-        formData.append("long", longitude);
-
-        try {
-          const response = await axios({
-            method: "post",
-            url: "http://127.0.0.1:4000/upload",
-            data: formData,
-            headers: { "Content-Type": "multipart/form-data" },
-          });
-
-          const data = response.data;
-          setCapturedImage({
-            image: imageData,
-            prediction: data.prediction,
-            latitude,
-            longitude,
-          });
-
-          setPrediction(data.prediction);
-          setShowModal(true);
-        } catch (error) {
-          console.error("Error uploading image:", error);
-        }
-      },
-      (error) => {
-        console.error("Error getting geolocation:", error);
-      }
-    );
+              setPrediction(data.prediction);
+              setShowModal(true);
+            } catch (error) {
+              console.error("Error uploading image:", error);
+            }
+          },
+          (error) => {
+            console.error("Error getting geolocation:", error);
+          }
+        );
+      };
+      reader.readAsDataURL(file);
+    }
   };
 
   const handleRetry = () => {
@@ -126,39 +99,31 @@ function VendorScanner() {
     return new Blob([ia], { type: mimeString });
   };
 
-  const getVendors = async () => {
-    const querySnapshot = await getDocs(collection(appFirestore, "VENDOR"));
-    let vendors = [];
-    querySnapshot.forEach((doc) => {
-      const ven = { id: doc.id, data: doc.data() };
-      vendors.push(ven);
-    });
-    return vendors;
-  };
-
   return (
-    <div className={styles.scannerContainer}>
-      <header className={styles.scannerHeader}>
+    <div className={styles.vendorScannerContainer}>
+      <header className={styles.vendorScannerHeader}>
         <h1>PlastiCart Scanner</h1>
       </header>
-      <div className={styles.scannerContent}>
-        <div className={styles.scannerVideoContainer}>
-          <h2>Hello Vendor, Please Scan your plastic ↓</h2>
-          <video ref={videoRef} width="500" height="340" muted autoPlay />
-          <button
-            className={styles.scannerCaptureButton}
-            onClick={handleCaptureClick}
-          ></button>
+      <div className={styles.vendorScannerContent}>
+        <div className={styles.vendorScannerVideoContainer}>
+          <h2>Hello Vendor, Please Capture your plastic image ↓</h2>
+          <input
+            type="file"
+            accept="image/*"
+            capture="environment"
+            onChange={handleImageUpload}
+            className={styles.vendorScannerFileInput}
+          />
         </div>
       </div>
       {showModal && (
-        <div className={styles.scannerModal}>
-          <div className={styles.scannerModalContent}>
+        <div className={styles.vendorScannerModal}>
+          <div className={styles.vendorScannerModalContent}>
             <h2>Verification Result</h2>
             <img
               src={capturedImage.image}
               alt="Current Capture"
-              className={styles.scannerModalImage}
+              className={styles.vendorScannerModalImage}
             />
             <p>Prediction: {prediction}</p>
             {verificationResult ? (
@@ -166,13 +131,13 @@ function VendorScanner() {
             ) : (
               <>
                 <button
-                  className={styles.scannerModalButton}
+                  className={styles.vendorScannerModalButton}
                   onClick={handleVerify}
                 >
                   Verify
                 </button>
                 <button
-                  className={styles.scannerModalButton}
+                  className={styles.vendorScannerModalButton}
                   onClick={handleRetry}
                 >
                   Retry
